abab646bfbec020c7794eb02e225fd75f454c65a
CF <qianqian.chen@lofty.com>
2025-08-04 21:10:51 +0800

    add seo and update nav href and fix mixpanel

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,8 +5,9 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-const trackMixPanel =
-  typeof (window as any).trackMixPanel === "function"
-    ? (window as any).trackMixPanel
-    : () => {};
+const trackMixPanel = (...args)=>{
+  if(typeof (window as any).trackMixPanel === "function") {
+    (window as any).trackMixPanel.apply(this, args)
+  }
+}
 export { trackMixPanel };
