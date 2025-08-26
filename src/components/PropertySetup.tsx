4dd8c4e5ac49867e4e1c16871c44ee0b35a46043
fangfang.zhang <fangfang.zhang3@chimeinc.com>
2025-08-26 08:39:45 +0800

    google api v1

--- src/components/PropertySetup.tsx
+++ src/components/PropertySetup.tsx
@@ -197,7 +197,7 @@ const PropertySetup: React.FC<PropertySetupProps> = ({
               <img src="https://cdn.lofty.com/image/fs/servicetool/2025824/13/original_03d678ea004049a5.png" style={{width: '36px', height: '36px'}} alt="" />
             </div>
           )}
-          <img src={image} alt={`Property at ${address}`} style={{ width: isMobile ? 100 : 133, height: "auto"}} />
+          <img src={image} alt={`Property at ${address}`} style={{ width: isMobile ? 100 : 133, height: "100"}} />
           <div style={{ padding: isMobile ? '0 0 0 10px' : '15px 20px 15px 15px', width: '100%'}}>
             <div className="text-sm font-medium" style={{ color: '#515666', lineHeight: isMobile ? '1.2':'20px', textAlign: 'left', lineClamp: 2, overflow: 'hidden',  textOverflow: 'ellipsis'}}>{address}</div>
             <div className="flex text-xs" style={{lineHeight: isMobile ? '12px' : '20px', color:'#797E8B', marginTop: isMobile ? '5px': 0}}>
